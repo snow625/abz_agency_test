731763289414b20b67db9b47cdcd9850310c19fd
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2023-05-29 11:54:10 +0300

    strictMode On

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from "./App.jsx";
 import "./sass/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
- <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+//  <App />
 
 );
